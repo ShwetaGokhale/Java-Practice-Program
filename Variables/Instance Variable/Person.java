7b3a2ce2b7e6f03a55713a219e5ffe846c5d2fc5
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-05 00:25:21 +0530

    A1- Code Of Instance Variable

---
+++ Variables/Instance Variable/Person.java
@@ -0,0 +1,16 @@
+class Person
+{
+	String name;
+	int age;
+	String address;
+	public static void main(String args[])
+	{
+		Person obj = new Person();
+                                     obj.name = "Shweta";
+		obj.age = 21;
+		obj.address ="Nagpur";
+		System.out.println(obj.name);
+		System.out.println(obj.age);
+		System.out.println(obj.address);
+	}
+}
