71e82ce723fdc3104e03f5829009d5a263a55c54
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-05 00:44:10 +0530

    A2- Code Of Arithmatic Operator

---
+++ Operators/Arithmatic Operator/Operator.java
@@ -0,0 +1,14 @@
+public class Operator
+{
+	public static void main(String args[])
+	{
+		int a = 2;
+		int b = 2;
+		int c = 1;
+		int d= 1;
+		System.out.println("Addition of two numbers = " +(a+b));
+		System.out.println("Subtraction of two numbers = " +(a-b));
+		System.out.println("Multiplication of two numbers = " +(a*b));
+		System.out.println("Division of two numbers = " +(a/b));
+	}
+}
---
+++ Operators/Arithmatic Operator/Rectangle.java
@@ -0,0 +1,10 @@
+public class Rectangle
+{
+	public static void main(String args[])
+	{
+		int length = 5;
+		int width = 5;
+		System.out.println("Area of Rectangle =  " +(length*width));
+		System.out.println("Perimeter of Rectangle =  " +(2*(length+width)));
+	}
+}
