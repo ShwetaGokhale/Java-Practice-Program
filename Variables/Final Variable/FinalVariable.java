49adcc88586c3014714cdf08e397c7df1a4e7b68
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-05 00:38:52 +0530

    A1- Code Of Final Variable

---
+++ Variables/Final Variable/FinalVariable.java
@@ -0,0 +1,13 @@
+class  FinalVariable
+{
+	public  static  void  main(String args[])
+	{
+		
+		final  float  PI  =  3.142f;
+		final  int  MAX_SIZE  =  12;
+		//PI  =  3.223f;
+		//MAX_SIZE  =  13;
+	                  System.out.println("PI  =  " +PI);
+		System.out.println("MAX SIZE   =  " +MAX_SIZE);
+	}
+}
