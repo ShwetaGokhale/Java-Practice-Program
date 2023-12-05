fc5323b200c9039ade311e2cdd4b6d35e6b47d48
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-05 13:32:19 +0530

    A2- Code Of Assignment Operator

---
+++ Operators/Assignment Operator/Interest.java
@@ -0,0 +1,15 @@
+public  class  Interest
+{
+	public  static  void  main(String args[])
+	{
+		int  si;
+		int  p  =  20;
+		int  r  =  10;
+		int  t  =  6;
+		si = (p*(1+r*t));
+		System.out.println("Principle  =  " +p);
+		System.out.println("Rate  =  " +p);
+		System.out.println("Time  =  " +p);
+		System.out.println("Simple Interest  =  " +si);
+	}
+}
---
+++ Operators/Assignment Operator/New.java
@@ -0,0 +1,15 @@
+public  class  New
+{
+	public static void main(String args[])
+	{
+		int   a  =  10;
+		a += 5;
+		System.out.println("a += 5 =  " + a);  
+		a -= 5;
+		System.out.println("a -= 5 =  " + a);  
+		a *= 5;
+		System.out.println("a *= 5 =  " + a);  
+		a /= 5;
+		System.out.println("a /= 5 =  " + a);  
+	}
+}
