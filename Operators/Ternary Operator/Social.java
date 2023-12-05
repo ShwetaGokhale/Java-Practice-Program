8b30c500e037a7582072d3977cd6fb237a321cba
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-05 13:40:10 +0530

    A2- Code Of Ternary Operator

---
+++ Operators/Ternary Operator/Social.java
@@ -0,0 +1,10 @@
+public  class  Social
+{
+	public  static  void  main(String args[])
+	{
+		int  i  = 40;
+		int  j  =  30;
+		String result  = i  >  j  ?   "Maximum"  :   "Minimum" ;
+		System.out.println("Number  is  =  " +result);
+	}
+}
