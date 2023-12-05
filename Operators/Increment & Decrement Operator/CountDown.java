de1dedc6968443308adc9409c255342549309a4b
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-05 13:35:29 +0530

    A2- Code Of Increment & Decrement Operator

---
+++ Operators/Increment & Decrement Operator/CountDown.java
@@ -0,0 +1,10 @@
+public  class  CountDown
+{
+	public  static  void  main(String[] args) 
+	{       
+        		int  i;
+		for(i  = 10;  i  > 0;  i--)
+      		System.out.println("i  =  "  +i);  
+	}
+
+}
---
+++ Operators/Increment & Decrement Operator/View.java
@@ -0,0 +1,14 @@
+public  class  View
+{
+	public  static  void  main(String[] args) 
+	{       
+        		int  x  =  10;       
+       	                  int  y  =  ++x;       
+      		System.out.println("Y  value  is =  " + y);  
+		
+		int  a  =  20;       
+       	                  int  b  =  a++;       
+      		System.out.println("B  value  is =  " + b);  
+	}
+
+}
