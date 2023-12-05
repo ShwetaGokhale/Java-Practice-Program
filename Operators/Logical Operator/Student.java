a77dbda4bb863a5440a5c3a18e1169d502ebf041
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-05 13:38:28 +0530

    A2- Code Of Logical Operator

---
+++ Operators/Logical Operator/Student.java
@@ -0,0 +1,16 @@
+public  class  Student
+{
+	public  static  void  main(String args[])
+	{
+		int  age  =  25;
+		int  percentage  =   68;
+		if(age<=22  &&  percentage<=68)
+		{
+			System.out.println("student is eligible for admission ");
+		}
+		else
+		{
+			System.out.println("student is not eligible for admission ");	
+		}
+	}
+}
