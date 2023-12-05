1783aeaaa1d70bda482dd9c9ca5f14f1a33118cf
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-05 15:15:04 +0530

    A2- Code Of Logical Operator

--- Operators/Logical Operator/One.java
+++ Operators/Logical Operator/One.java
@@ -1,24 +1,15 @@
 public  class  One
-//{
-//	public  static  void  main(String args[])
-//	{
-//		int  a  =  23;
-//		if(a%2==0  &&  a%3==0)
-//		{
-//			System.out.println("Number is divisible");
-//		}
-//		else
-//		{
-//			System.out.println("Number is not divisible");	
-//		}
-//	}
-//}
-//public  class  LeapYear
 {
 	public  static  void  main(String args[])
 	{
 		int  a  =  23;
-		System.out.println("Number is divisible  =   " +(a%2==0));
-		System.out.println("Number is not divisible  =   " +(a%3==0));
+		if(a%2==0  &&  a%3==0)
+		{
+			System.out.println("Number is divisible");
+		}
+		else
+		{
+			System.out.println("Number is not divisible");	
+		}
 	}
 }
