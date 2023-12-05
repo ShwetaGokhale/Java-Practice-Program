73ab8cd0af6db9ae943166aed892df7b6e6d978d
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-05 16:11:19 +0530

    A2- Code Of Relational Operator

---
+++ Operators/Relational Operator/TwoNumbers.java
@@ -0,0 +1,34 @@
+public class TwoNumbers
+{
+	public static void main(String args[])
+	{
+		int  a  =  2;
+		int  b  =  4;
+		System.out.println(a);
+		System.out.println(b);
+		if(a > b)
+		{
+			System.out.println("First  number is greater than second number");
+		}
+		else
+		{
+			System.out.println("First  number is not greater than second number");
+		}
+		if(a < b)
+		{
+			System.out.println("First  number is less than second number");
+		}
+		else
+		{
+			System.out.println("First  number is not less than second number");
+		}
+		if(a == b)
+		{
+			System.out.println("First  number is equal to second number");
+		}
+		else
+		{
+			System.out.println("First  number is not equal  to  second number");
+		}
+	}
+}
