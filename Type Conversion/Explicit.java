d47ba4d034ccab88c474a142dab04d1d48b66307
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-06 14:23:24 +0530

    Code Of Type Conversion

---
+++ Type Conversion/Explicit.java
@@ -0,0 +1,10 @@
+public   class   Explicit
+{
+	public static void main(String args[])
+	{
+		short   a  =  40;
+		byte   b  =  (byte) a;
+		System.out.println(a);
+		System.out.println(b);		
+	}
+}
---
+++ Type Conversion/Implicit.java
@@ -0,0 +1,10 @@
+public   class   Implicit
+{
+	public static void main(String args[])
+	{
+		byte   a  =  50;
+		short   b  =  a;
+		System.out.println(a);
+		System.out.println(b);		
+	}
+}
