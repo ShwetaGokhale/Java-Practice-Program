801577618c1242665390e68096b4d321c7f94e4a
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-11 00:22:04 +0530

    Basic Code Of Assignment Operator

---
+++ Operators/Assignment.java
@@ -0,0 +1,14 @@
+class   Assignment 
+{
+	public   static   void   main(String  args[])
+	{
+		int   num;
+		String   name;
+
+		num   =   60;
+		name   =   "BootCoding";
+
+		System.out.println("Num  =  "  + num);
+		System.out.println("Name   =   "   + name);
+	}
+}
