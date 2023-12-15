2b178d93f751bd17913318497b9973d927ce87c3
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-15 15:16:35 +0530

    Number Increasing Pyramid Shape Code

---
+++ Control Statements/Looping Statements/Nested For Loop/NumberIncreasingPyramid.java
@@ -0,0 +1,15 @@
+class NumberIncreasingPyramid
+{
+	public static void main(String args[])
+	{
+		for(int b = 1; b <= 5; b++)
+		{
+			for(int d = 1; d <= b; d++)
+			{
+				System.out.print(d + " ");
+			}
+		System.out.println();
+		}
+		
+	}
+}
