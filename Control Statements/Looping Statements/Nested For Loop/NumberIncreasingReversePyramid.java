60fbef4db1b919328dd12fc5d74d7b6419306d7c
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-15 15:37:46 +0530

    Number Increasing Reverse Pyramid Shape Code

---
+++ Control Statements/Looping Statements/Nested For Loop/NumberIncreasingReversePyramid.java
@@ -0,0 +1,15 @@
+class NumberIncreasingReversePyramid
+{
+	public static void main(String args[])
+	{
+		for(int b = 5; b >= 1; b--)
+		{
+			for(int d = 1; d <= b; d++)
+			{
+				System.out.print(d + " ");
+			}
+		System.out.println();
+		}
+		
+	}
+}
