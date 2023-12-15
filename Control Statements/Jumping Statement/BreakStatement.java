68735b438618b140c20043925877b6d6ede3996a
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-15 13:10:35 +0530

    Basic Code For Break Statement

---
+++ Control Statements/Jumping Statement/BreakStatement.java
@@ -0,0 +1,17 @@
+//Program to print number from 1 to 10.
+
+
+class BreakStatement
+{
+	public static void main(String args[])
+	{
+		for(int k = 1; k <= 10; k++)
+		{
+			if(k == 5)
+			{
+				break;
+			}
+			System.out.println(k);
+		}
+	}
+}
