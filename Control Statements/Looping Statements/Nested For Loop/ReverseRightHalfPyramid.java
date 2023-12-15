b3a3821f6a40b4fec681e977db7dc5edac2dc253
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-15 16:14:10 +0530

    Reverse Right Half Pyramid Shape Code

---
+++ Control Statements/Looping Statements/Nested For Loop/ReverseRightHalfPyramid.java
@@ -0,0 +1,15 @@
+class ReverseRightHalfPyramid
+{
+	public static void main(String args[])
+	{
+		for(int b = 5; b >= 1; b--)
+		{
+			for(int d = 1; d <= b; d++)
+			{
+				System.out.print(" * ");
+			}
+		System.out.println();
+		}
+		
+	}
+}
