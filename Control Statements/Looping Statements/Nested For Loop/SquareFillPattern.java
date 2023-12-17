7020ba4c011dcead6bf818f9a3b087345fac7a9d
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-18 01:33:30 +0530

    Square Fill Pattern Code

--- Control Statements/Looping Statements/Nested For Loop/SquareFillPattern.java
+++ Control Statements/Looping Statements/Nested For Loop/SquareFillPattern.java
@@ -2,9 +2,10 @@ class SquareFillPattern
 {
 	public static void main(String args[])
 	{
-		for(int b = 1; b <= 5; b++)
+		
+		for(int  b  = 1;  b  <=  5;  b++)
 		{
-			for(int d = 1; d <= 5; d++)
+			for(int  d = 1;  d  <=  5;  d++)
 			{
 				System.out.print(" * ");
 			}
