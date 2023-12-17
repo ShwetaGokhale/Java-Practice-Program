cd2de496fd6eaf4be0c8236f417ad834d2c759dd
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-18 04:09:33 +0530

    Code Of Multiplication Table

---
+++ Control Statements/Looping Statements/While Loop/MultiplicationTable.java
@@ -0,0 +1,17 @@
+//Multiplication Table:
+//Create a program that prints the multiplication table of a given number using a while loop. Print the first 10 multiples.
+
+
+class   MultiplicationTable
+{
+	public   static   void   main(String args[])
+	{
+		int b = 1;
+		System.out.println("Multiplication Table = ");
+		while(b <= 10)
+		{
+			System.out.println(b*7);
+			b++;
+		}
+	} 
+}
